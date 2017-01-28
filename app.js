03dda59cc470f911fedb665646e36e714a379dfc
Drake Scott <drakealexscott@gmail.com>
2017-01-27 23:22:53 -0600

    first trryyy! Got all these simple exercises done.

---
+++ app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        name: 'Drake',
+        age: 19,
+        image: "https://queerty-prodweb.s3.amazonaws.com/2017/01/14343_Netherlands_Students.jpg"
+    }
+})
